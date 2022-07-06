dfc8cb533d2490444914742ad79e2eb824686392
AndreyShukatka <106096891+AndreyShukatka@users.noreply.github.com>
2022-07-06 15:52:22 +0300

    publish photo to Telegram channel.

--- telegram_bot.py
+++ telegram_bot.py
@@ -3,7 +3,8 @@ import os
 from dotenv import load_dotenv
 
 load_dotenv()
+chat_id = '@SpacePhotobyShukatka'
 token = os.environ['TELEGRAM_TOKEN']
 bot = telegram.Bot(token=token)
 updates = bot.get_updates()
-bot.send_message(chat_id='@SpacePhotobyShukatka', text='Привет')
+bot.send_document(chat_id = chat_id, document = open('images/epic_Nasa_2022_06_30_00_03.png', 'rb'))
