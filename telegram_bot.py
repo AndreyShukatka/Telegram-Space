8a17a84fdf5a7ba2e63f4646e12f6d6eaa928d76
AndreyShukatka <106096891+AndreyShukatka@users.noreply.github.com>
2022-07-06 16:51:32 +0300

    Добавлен скрипт отправки фото с нужным интервалом

--- telegram_bot.py
+++ telegram_bot.py
@@ -1,10 +1,35 @@
-import telegram
+import argparse
 import os
+import random
+import time
 from dotenv import load_dotenv
 
-load_dotenv()
-chat_id = '@SpacePhotobyShukatka'
-token = os.environ['TELEGRAM_TOKEN']
-bot = telegram.Bot(token=token)
-updates = bot.get_updates()
-bot.send_document(chat_id = chat_id, document = open('images/epic_Nasa_2022_06_30_00_03.png', 'rb'))
+import telegram
+
+
+def create_parser():
+    parser = argparse.ArgumentParser(description='Программа отправляет фотографии в Телеграмм канал с заданной интенсивностью')
+    parser.add_argument('-t', help='укажите количество секунд, которое необходимо для задержки отправления фото', default='14400', type = int)
+    args = parser.parse_args()
+    return args
+
+def send_message_bot(chat_id, token, delay_time):
+    bot = telegram.Bot(token=token)
+    filesindir = os.walk('images')
+    for root, dirs, files in filesindir:
+        name_pictures = files
+        while True:
+            picture_download = open(f'images/{random.choice(name_pictures)}', 'rb')
+            bot.send_photo(chat_id=chat_id, photo=picture_download)
+            picture_download.close()
+            time.sleep(delay_time)
+
+
+if __name__ == '__main__':
+    load_dotenv()
+    chat_id = '@SpacePhotobyShukatka'
+    token = os.environ['TELEGRAM_TOKEN']
+    delay_time = create_parser().t
+    print(delay_time)
+    send_message_bot(chat_id, token, delay_time)
+
