826a2a67a77726dce055fa14388474252b3c8ae8
AndreyShukatka <106096891+AndreyShukatka@users.noreply.github.com>
2022-07-06 15:43:43 +0300

    Файл с функциями скачивания фото космоса Nasa

---
+++ fetch_nasa_images.py
@@ -0,0 +1,33 @@
+import argparse
+import os
+from dotenv import load_dotenv
+import requests
+from downloads_images import extract_file_extension, download_image
+
+
+def create_parser():
+    parser = argparse.ArgumentParser(description='Программа скачивает фотографии космоса Nasa')
+    parser.add_argument('-c', help='Количество фото', default='5')
+    args =parser.parse_args()
+    return args
+
+
+def nasa_download_image(nasa_token, count):
+    nasa_url = 'https://api.nasa.gov/planetary/apod'
+    params = {'api_key':nasa_token, 'count':count}
+    response = requests.get(nasa_url, params=params)
+    response.raise_for_status()
+    url = response.json()
+    for url_number, data in enumerate(url):
+        url = data['url']
+        file_name = 'nasa_appod_{}{}'.format(url_number,
+                                             extract_file_extension (url))
+        download_image(url,
+                       file_name)
+
+
+if __name__ == '__main__':
+    load_dotenv()
+    nasa_token = os.environ['TOKEN_NASA']
+    count = create_parser().c
+    nasa_download_image(nasa_token, count)
