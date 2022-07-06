841864bb44357ffcf90d5cd554c391825fca273b
AndreyShukatka <106096891+AndreyShukatka@users.noreply.github.com>
2022-07-06 22:45:08 +0300

    Убрал повторение фото в одном цикле

--- telegram_bot.py
+++ telegram_bot.py
@@ -9,7 +9,7 @@ import telegram
 
 def create_parser():
     parser = argparse.ArgumentParser(description='Программа отправляет фотографии в Телеграмм канал с заданной интенсивностью')
-    parser.add_argument('-t', help='укажите количество секунд, которое необходимо для задержки отправления фото', default='14400', type = int)
+    parser.add_argument('-t', help='укажите количество секунд, которое необходимо для задержки отправления фото', default='10', type = int)
     parser.add_argument('-id', help='Указать id, на который необходимо посылать фотографии', default='@SpacePhotobyShukatka')
     args = parser.parse_args()
     return args
@@ -18,19 +18,23 @@ def send_message_bot(chat_id, token, delay_time):
     bot = telegram.Bot(token=token)
     filesindir = os.walk('images')
     for root, dirs, files in filesindir:
-        name_pictures = files
+        name_pictures = files.copy()
         while True:
-            picture_download = open(f'images/{random.choice(name_pictures)}', 'rb')
-            bot.send_photo(chat_id=chat_id, photo=picture_download)
-            picture_download.close()
-            time.sleep(delay_time)
+            if len(name_pictures) >= 1:
+                picture_download = open(f'images/{random.choice(name_pictures)}', 'rb')
+                bot.send_photo(chat_id=chat_id, photo=picture_download)
+                picture_download.close()
+                name_pictures.remove(picture_download.name.split('/')[1])
+                time.sleep(delay_time)
+            elif len(name_pictures) < 1:
+                name_pictures = files.copy()
+                random.shuffle(name_pictures)
 
 
 if __name__ == '__main__':
     load_dotenv()
-    chat_id = create_parser().id
+    chat_id = '@SpacePhotobyShukatka'
     token = os.environ['TELEGRAM_TOKEN']
     delay_time = create_parser().t
-    print(delay_time)
     send_message_bot(chat_id, token, delay_time)
 
