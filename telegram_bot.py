8d83aa9863cbd693c15e8740d20937c8b2dd5ca7
AndreyShukatka <106096891+AndreyShukatka@users.noreply.github.com>
2022-07-06 17:13:24 +0300

    Добавлен аргумент ком. строки -id

--- telegram_bot.py
+++ telegram_bot.py
@@ -10,9 +10,9 @@ import telegram
 def create_parser():
     parser = argparse.ArgumentParser(description='Программа отправляет фотографии в Телеграмм канал с заданной интенсивностью')
     parser.add_argument('-t', help='укажите количество секунд, которое необходимо для задержки отправления фото', default='14400', type = int)
+    parser.add_argument('-id', help='Указать id, на который необходимо посылать фотографии', default='@SpacePhotobyShukatka')
     args = parser.parse_args()
     return args
-
 
 def send_message_bot(chat_id, token, delay_time):
     bot = telegram.Bot(token=token)
@@ -20,15 +20,17 @@ def send_message_bot(chat_id, token, delay_time):
     for root, dirs, files in filesindir:
         name_pictures = files
         while True:
-            picture_sending = open(f'images/{random.choice(name_pictures)}', 'rb')
-            bot.send_photo(chat_id=chat_id, photo=picture_sending)
-            picture_sending.close()
+            picture_download = open(f'images/{random.choice(name_pictures)}', 'rb')
+            bot.send_photo(chat_id=chat_id, photo=picture_download)
+            picture_download.close()
             time.sleep(delay_time)
 
 
 if __name__ == '__main__':
     load_dotenv()
-    chat_id = '@SpacePhotobyShukatka'
+    chat_id = create_parser().id
     token = os.environ['TELEGRAM_TOKEN']
     delay_time = create_parser().t
+    print(delay_time)
     send_message_bot(chat_id, token, delay_time)
+
