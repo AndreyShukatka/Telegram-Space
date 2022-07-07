925d81e6a8cf9d23728229157fdb3a30271f85ec
AndreyShukatka <106096891+AndreyShukatka@users.noreply.github.com>
2022-07-07 23:33:10 +0300

    Поправил наименование переменных

--- telegram_bot.py
+++ telegram_bot.py
@@ -15,23 +15,23 @@ def create_parser():
     return args
 
 def create_list_pictures():
-    path_pictures = list()
+    pictures_path = list()
     for root, dir, file in os.walk('images'):
-        for name_picture in file:
-            path_pictures.append(os.path.join(root, name_picture))
-    return path_pictures
+        for picture_name in file:
+            pictures_path.append(os.path.join(root, picture_name))
+    return pictures_path
 
-def send_message_bot(chat_id, token, delay_time):
+def send_photo(chat_id, token, delay_time, pictures_path):
     bot = telegram.Bot(token=token)
-        while True:
-            if len(name_pictures) >= 1:
-                with open (random.choice(path_pictures), 'rb') as pictures:
-                    bot.send_photo(chat_id, pictures)
-                path_pictures.remove(pictures.name)
-                time.sleep(delay_time)
-            elif len(name_pictures) < 1:
-                name_pictures = files.copy()
-                random.shuffle(name_pictures)
+    while True:
+        if len(pictures_path) >= 1:
+            with open (random.choice(pictures_path), 'rb') as pictures:
+                bot.send_photo(chat_id, pictures)
+            pictures_path.remove(pictures.name)
+            time.sleep(delay_time)
+        elif len(pictures_path) < 1:
+            pictures_path = files.copy()
+            random.shuffle(name_pictures)
 
 
 if __name__ == '__main__':
