38ecf6d5ae45735fbe29a8a1837a3e136d9d6ccf
AndreyShukatka <106096891+AndreyShukatka@users.noreply.github.com>
2022-07-08 01:48:22 +0300

    исправил название переменной

--- fetch_nasa_images.py
+++ fetch_nasa_images.py
@@ -1,35 +1,33 @@
 import argparse
 import os
 from dotenv import load_dotenv
-
 import requests
-
 from downloads_images import extract_file_extension, download_image
 
 
-def parsing_input_command_line():
+def create_parser():
     parser = argparse.ArgumentParser(description='Программа скачивает фотографии космоса Nasa')
     parser.add_argument('-c', help='Количество фото', default='5')
     args =parser.parse_args()
     return args
 
 
-def download_nasa_image(nasa_token, count):
+def nasa_download_image(nasa_token, count):
     nasa_url = 'https://api.nasa.gov/planetary/apod'
     params = {'api_key':nasa_token, 'count':count}
     response = requests.get(nasa_url, params=params)
     response.raise_for_status()
-    url = response.json()
-    for url_number, data in enumerate(url):
+    photo_information = response.json()
+    for url_number, data in enumerate(photo_information):
         url = data['url']
         file_name = 'nasa_appod_{}{}'.format(url_number,
                                              extract_file_extension (url))
         download_image(url,
-                       file_name)
+                       file_name, nasa_token)
 
 
 if __name__ == '__main__':
     load_dotenv()
     nasa_token = os.environ['TOKEN_NASA']
-    count = parsing_input_command_line().c
-    download_nasa_image(nasa_token, count)
+    count = create_parser().c
+    nasa_download_image(nasa_token, count)
