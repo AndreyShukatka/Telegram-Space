4f5fbdf689e8ff5b8885a4eb6110b5ec6b3b7847
AndreyShukatka <106096891+AndreyShukatka@users.noreply.github.com>
2022-07-09 00:06:09 +0300

    Поправил функцию

--- fetch_spacex_images.py
+++ fetch_spacex_images.py
@@ -3,7 +3,7 @@ import argparse
 from downloads_images import extract_file_extension, download_image
 
 
-def input_parsing_command_line():
+def parsing_input_command_line():
     parser = argparse.ArgumentParser(
         description='Программа скачивает фотографии запуска SpaceX'
     )
@@ -14,8 +14,8 @@ def input_parsing_command_line():
 
 def fetch_spacex_last_launch(id_space):
     spacex_url = 'https://api.spacexdata.com/v3/launches/'
-    payload = {'flight_number': id_space}
-    response = requests.get(spacex_url, params=payload)
+    params = {'flight_number': id_space}
+    response = requests.get(spacex_url, params=params)
     response.raise_for_status()
     response_data = response.json()[0]['links']['flickr_images']
     flight_number = response.json()[0]['flight_number']
@@ -26,9 +26,10 @@ def fetch_spacex_last_launch(id_space):
             extract_file_extension(url)
         )
         download_image(url,
-                       file_name)
+                       file_name,
+                       params)
 
 
 if __name__ == '__main__':
-    id_space = input_parsing_command_line().id
+    id_space = parsing_input_command_line().id
     fetch_spacex_last_launch(id_space)
