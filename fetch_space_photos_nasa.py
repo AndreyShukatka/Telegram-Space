5d5e7677390345735f7d83e9c6e938b6e002f0c8
AndreyShukatka <106096891+AndreyShukatka@users.noreply.github.com>
2022-07-08 23:24:05 +0300

    Исправил название переменной

--- fetch_space_photos_nasa.py
+++ fetch_space_photos_nasa.py
@@ -5,7 +5,7 @@ import requests
 from downloads_images import extract_file_extension, download_image
 
 
-def create_parser():
+def input_parsing_command_line():
     parser = argparse.ArgumentParser(
         description='Программа скачивает фотографии космоса Nasa'
     )
@@ -18,7 +18,7 @@ def create_parser():
     return args
 
 
-def nasa_download_image(nasa_token, count):
+def nasa_download_image(nasa_token, count_photos):
     nasa_url = 'https://api.nasa.gov/planetary/apod'
     params = {'api_key': nasa_token, 'count': count}
     response = requests.get(nasa_url, params=params)
@@ -39,5 +39,5 @@ def nasa_download_image(nasa_token, count):
 if __name__ == '__main__':
     load_dotenv()
     nasa_token = os.environ['TOKEN_NASA']
-    count = create_parser().c
+    count_photos = input_parsing_command_line().c
     nasa_download_image(nasa_token, count)
