a1d1393486140398ff905909662a8341abd85901
AndreyShukatka <106096891+AndreyShukatka@users.noreply.github.com>
2022-07-09 00:05:22 +0300

    Поправил функцию

--- fetch_space_photos_nasa.py
+++ fetch_space_photos_nasa.py
@@ -5,7 +5,7 @@ import requests
 from downloads_images import extract_file_extension, download_image
 
 
-def input_parsing_command_line():
+def create_parser():
     parser = argparse.ArgumentParser(
         description='Программа скачивает фотографии космоса Nasa'
     )
@@ -18,7 +18,7 @@ def input_parsing_command_line():
     return args
 
 
-def nasa_download_image(nasa_token, count_photos):
+def nasa_download_image(nasa_token, count):
     nasa_url = 'https://api.nasa.gov/planetary/apod'
     params = {'api_key': nasa_token, 'count': count}
     response = requests.get(nasa_url, params=params)
@@ -32,12 +32,13 @@ def nasa_download_image(nasa_token, count_photos):
         )
         download_image(
             url,
-            file_name
+            file_name,
+            params
         )
 
 
 if __name__ == '__main__':
     load_dotenv()
     nasa_token = os.environ['TOKEN_NASA']
-    count_photos = input_parsing_command_line().c
+    count = create_parser().c
     nasa_download_image(nasa_token, count)
