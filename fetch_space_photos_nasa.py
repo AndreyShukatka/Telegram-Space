0fccdd553ff18541c08767fc13d6e0717050bebd
AndreyShukatka <106096891+AndreyShukatka@users.noreply.github.com>
2022-07-08 21:50:35 +0300

    Исправить PEP8

--- fetch_space_photos_nasa.py
+++ fetch_space_photos_nasa.py
@@ -6,24 +6,34 @@ from downloads_images import extract_file_extension, download_image
 
 
 def create_parser():
-    parser = argparse.ArgumentParser(description='Программа скачивает фотографии космоса Nasa')
-    parser.add_argument('-c', help='Количество фото', default='5')
-    args =parser.parse_args()
+    parser = argparse.ArgumentParser(
+        description='Программа скачивает фотографии космоса Nasa'
+    )
+    parser.add_argument(
+        '-c',
+        help='Количество фото',
+        default='5'
+    )
+    args = parser.parse_args()
     return args
 
 
 def nasa_download_image(nasa_token, count):
     nasa_url = 'https://api.nasa.gov/planetary/apod'
-    params = {'api_key':nasa_token, 'count':count}
+    params = {'api_key': nasa_token, 'count': count}
     response = requests.get(nasa_url, params=params)
     response.raise_for_status()
     photo_information = response.json()
     for number, data in enumerate(photo_information):
         url = data['url']
-        file_name = 'nasa_appod_{}{}'.format(url_number,
-                                             extract_file_extension (url))
-        download_image(url,
-                       file_name)
+        file_name = 'nasa_appod_{}{}'.format(
+            number,
+            extract_file_extension(url)
+        )
+        download_image(
+            url,
+            file_name
+        )
 
 
 if __name__ == '__main__':
