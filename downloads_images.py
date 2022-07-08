0f2c1ff24a5ac5674cc3a6c612fcd8dfebfde8ed
AndreyShukatka <106096891+AndreyShukatka@users.noreply.github.com>
2022-07-09 00:04:29 +0300

    Убрал переменную

--- downloads_images.py
+++ downloads_images.py
@@ -2,20 +2,14 @@ import os
 import pathlib
 import urllib
 import requests
-from dotenv import load_dotenv
 
 
-def download_image(url, file_name):
-    load_dotenv()
-    nasa_token = os.environ['TOKEN_NASA']
-    params = {'api_key': nasa_token}
+def download_image(url, file_name, params):
     img = requests.get(url, params=params)
     img.raise_for_status()
     folder_name = 'images/{}'.format(file_name)
-    pathlib.Path('images').mkdir(
-        parents=True,
-        exist_ok=True
-    )
+    pathlib.Path('images').mkdir(parents=True,
+                                 exist_ok=True)
     with open(folder_name, 'wb') as file:
         file.write(img.content)
 
