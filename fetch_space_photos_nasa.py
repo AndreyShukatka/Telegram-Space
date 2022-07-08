30301d4065360103c9445e00d303a1c18f6610b3
AndreyShukatka <106096891+AndreyShukatka@users.noreply.github.com>
2022-07-09 01:00:47 +0300

    Update fetch_space_photos_nasa.py

--- fetch_space_photos_nasa.py
+++ fetch_space_photos_nasa.py
@@ -18,9 +18,9 @@ def input_parsing_command_line():
     return args
 
 
-def nasa_download_image(nasa_token, count):
+def nasa_download_image(nasa_token, count_photos):
     nasa_url = 'https://api.nasa.gov/planetary/apod'
-    params = {'api_key': nasa_token, 'count': count}
+    params = {'api_key': nasa_token, 'count': count_photos}
     response = requests.get(nasa_url, params=params)
     response.raise_for_status()
     photo_information = response.json()
@@ -40,5 +40,5 @@ def nasa_download_image(nasa_token, count):
 if __name__ == '__main__':
     load_dotenv()
     nasa_token = os.environ['TOKEN_NASA']
-    count = input_parsing_command_line().c
-    nasa_download_image(nasa_token, count)
+    count_photos = input_parsing_command_line().c
+    nasa_download_image(nasa_token, count_photos)
