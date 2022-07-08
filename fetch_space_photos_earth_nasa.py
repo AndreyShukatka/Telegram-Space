8592b72aa3b0e968649e4adfce4349fc777d8c2d
AndreyShukatka <106096891+AndreyShukatka@users.noreply.github.com>
2022-07-08 21:45:27 +0300

    Исправил PEP8

--- fetch_space_photos_earth_nasa.py
+++ fetch_space_photos_earth_nasa.py
@@ -2,16 +2,22 @@ from datetime import datetime
 import requests
 import argparse
 import os
-import urllib
 from dotenv import load_dotenv
 from downloads_images import download_image
 
 
 def create_parser():
-    parser = argparse.ArgumentParser(description='Программа скачивает фотографии земли из космоса Nasa')
-    parser.add_argument('-с', help='Количество фото', default='5', type=int)
-    args =parser.parse_args()
+    parser = argparse.ArgumentParser(
+        description='Программа скачивает фотографии земли из космоса Nasa'
+    )
+    parser.add_argument('-с',
+                        help='Количество фото',
+                        default='5',
+                        type=int
+                        )
+    args = parser.parse_args()
     return args
+
 
 def download_epic_photo(nasa_token, epic_photos_amount):
     nasa_url = 'https://api.nasa.gov/EPIC/api/natural/'
@@ -23,12 +29,18 @@ def download_epic_photo(nasa_token, epic_photos_amount):
     flight_database = response.json()
     for day in range(epic_photos_amount):
         epic_date = datetime.fromisoformat(flight_database[day]['date'])
-        url = '{}{}/png/{}.png'.format(epic_base_url,
-                                        epic_date.strftime('%Y/%m/%d'),
-                                        flight_database[day]['image'])
-        file_name = 'epic_Nasa_{}.png'.format(datetime.fromisoformat(flight_database[day]
-                                                                     ['date']).strftime('%Y_%m_%d_%H_%M'))
+        url = '{}{}/png/{}.png'.format(
+            epic_base_url,
+            epic_date.strftime('%Y/%m/%d'),
+            flight_database[day]['image']
+        )
+        file_name = 'epic_Nasa_{}.png'.format(
+            datetime.fromisoformat(
+                flight_database[day]['date']
+            ).strftime('%Y_%m_%d_%H_%M')
+        )
         download_image(url, file_name)
+
 
 if __name__ == '__main__':
     load_dotenv()
