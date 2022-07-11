537d437645ebadbcf16394e5ad41eb95f03f01b0
AndreyShukatka <106096891+AndreyShukatka@users.noreply.github.com>
2022-07-11 15:11:46 +0300

    Update downloads_images.py

--- downloads_images.py
+++ downloads_images.py
@@ -1,14 +1,13 @@
 import os
 import pathlib
 import requests
-from urllib.parse import unquote
 from urllib.parse import urlsplit
 
 
 def download_image(url, file_name, params):
     response = requests.get(url, params=params)
     response.raise_for_status()
-    folder_name = 'images/{}'.format(file_name)
+    folder_name = os.path.join('images', file_name)
     pathlib.Path('images').mkdir(parents=True,
                                  exist_ok=True)
     with open(folder_name, 'wb') as file:
