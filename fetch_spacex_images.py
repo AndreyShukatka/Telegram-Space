d8650d2ea5375c20faa2142bc01003641d081f3e
AndreyShukatka <106096891+AndreyShukatka@users.noreply.github.com>
2022-07-11 14:31:29 +0300

    Исправил функцию

--- fetch_spacex_images.py
+++ fetch_spacex_images.py
@@ -13,12 +13,12 @@ def input_parsing_command_line():
 
 
 def fetch_spacex_last_launch(space_id):
-    spacex_url = 'https://api.spacexdata.com/v3/launches/'
-    params = {'flight_number': space_id}
+    spacex_url = f'https://api.spacexdata.com/v5/launches/{space_id}'
+    params = {}
     response = requests.get(spacex_url, params=params)
     response.raise_for_status()
-    response_data = response.json()[0]['links']['flickr_images']
-    flight_number = response.json()[0]['flight_number']
+    response_data = response.json()['links']['flickr']['original']
+    flight_number = response.json()['flight_number']
     for url_number, url in enumerate(response_data):
         file_name = 'spacex{}_{}{}'.format(
             flight_number,
