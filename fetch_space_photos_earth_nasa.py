548c54a27a4200e2757b082a51f9ecce59454b0a
AndreyShukatka <106096891+AndreyShukatka@users.noreply.github.com>
2022-07-11 16:03:53 +0300

    Высушил название переменных

--- fetch_space_photos_earth_nasa.py
+++ fetch_space_photos_earth_nasa.py
@@ -19,31 +19,31 @@ def input_parsing_command_line():
     return args
 
 
-def download_epic_photo(token_nasa, epic_photos_amount):
-    nasa_url = 'https://api.nasa.gov/EPIC/api/natural/'
-    epic_base_url = 'https://api.nasa.gov/EPIC/archive/natural/'
-    params = {'api_key': token_nasa}
-    response = requests.get(nasa_url,
+def download_epic_photo(token, amount):
+    url = 'https://api.nasa.gov/EPIC/api/natural/'
+    base_url = 'https://api.nasa.gov/EPIC/archive/natural/'
+    params = {'api_key': token}
+    response = requests.get(url,
                             params=params)
     response.raise_for_status()
     flight_information = response.json()
-    for day in range(epic_photos_amount):
-        epic_date = datetime.fromisoformat(flight_information[day]['date'])
+    for day in range(amount):
+        date = datetime.fromisoformat(flight_information[day]['date'])
         url = '{}{}/png/{}.png'.format(
-            epic_base_url,
-            epic_date.strftime('%Y/%m/%d'),
+            base_url,
+            date.strftime('%Y/%m/%d'),
             flight_information[day]['image']
         )
-        file_name = 'epic_Nasa_{}.png'.format(
+        name = 'epic_Nasa_{}.png'.format(
             datetime.fromisoformat(
                 flight_information[day]['date']
             ).strftime('%Y_%m_%d_%H_%M')
         )
-        download_image(url, file_name, params)
+        download_image(url, name, params)
 
 
 if __name__ == '__main__':
     load_dotenv()
-    token_nasa = os.environ['TOKEN_NASA']
-    epic_photos_amount = input_parsing_command_line().с
-    download_epic_photo(token_nasa, epic_photos_amount)
+    token = os.environ['TOKEN_NASA']
+    amount = input_parsing_command_line().с
+    download_epic_photo(token, amount)
